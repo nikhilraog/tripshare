264bf41040d8da59b7e4a10132af18d1f7109a3d
Nikhil G Rao <nikhilraog@gmail.com>
2014-11-25 18:59:38 -0600

    Removed tripville directory temporarily

--- Nikhil/tripville/src/main/java/com/github/tripville/service/CarRegService.java
+++
@@ -1,7 +0,0 @@
-package com.github.tripville.service;
-
-import com.github.tripville.model.MemberCar;
-
-public interface CarRegService {
-	MemberCar save(MemberCar car);
-}
--- Nikhil/tripville/src/main/java/com/github/tripville/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.github.tripville.service;
-
-import com.github.tripville.model.Member;
-
-public interface StudentService {
-	Member save(Member student);
-	boolean findByLogin(String userName, String password);
-	boolean findByUserName(String userName);
-}
